65dd12e1ff9c3ea2003c0632394ecc2f8d89f8e1
Scott Finlay <xisifer@gmail.com>
2019-10-11 20:10:23 -0500

    Basic skeleton setup with AJAXFormDelete pasted on top of my own intial efforts.

---
+++ client/src/components/BookSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+class BookSearch extends Component {
+    render() {
+        return (
+            <div>HELLO WORLD, I AM THE BOOK SEARCH SECTION</div>
+        )
+    }
+}
+export default BookSearch;
---
+++ client/src/components/SavedBooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+class SavedBooks extends Component {
+    render() {
+        return (
+            <div>HELLO WORLD, I AM THE SAVED BOOKS SECTION</div>
+        )
+    }
+}
+export default SavedBooks;
